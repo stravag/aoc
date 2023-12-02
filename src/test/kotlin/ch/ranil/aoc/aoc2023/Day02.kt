8062ac8a7ac341c89b7cb54a8bc0090ed0112465
Ranil Wijeyratne <me@ranil.ch>
2023-12-02 09:07:11 +0100

    2023 day02.1 parsing

--- src/test/kotlin/ch/ranil/aoc/aoc2023/Day02.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2023/Day02.kt
@@ -8,8 +8,26 @@ class Day02 : AbstractDay() {
 
     @Test
     fun part1() {
-        assertEquals(0, compute1(testInput))
+        assertEquals(8, compute1(testInput))
         assertEquals(0, compute1(puzzleInput))
+    }
+
+    @Test
+    fun part1Dummy() {
+        assertEquals(6 to "blue", " 6 blue ".parseCube())
+        assertEquals(listOf(3 to "blue", 4 to "red"), " 3 blue, 4 red".parseSubset())
+        assertEquals(
+            listOf(
+                listOf(6 to "red", 1 to "blue", 3 to "green"),
+                listOf(2 to "blue", 1 to "red", 2 to "green"),
+            ), " 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".parseSubsets()
+        )
+        assertEquals(
+            5 to listOf(
+                listOf(6 to "red", 1 to "blue", 3 to "green"),
+                listOf(2 to "blue", 1 to "red", 2 to "green"),
+            ), "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green".parseGame()
+        )
     }
 
     @Test
@@ -19,8 +37,46 @@ class Day02 : AbstractDay() {
     }
 
     private fun compute1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { it.parseGame() }
+            .filter { (_, possibleSubsets) ->
+                possibleSubsets.isNotEmpty()
+            }
+            .sumOf { it.first }
     }
+
+    private fun String.parseGame(): Pair<Int, List<List<Pair<Int, String>>>> {
+        val (gameNum, cubes) = this
+            .replace("Game ", "")
+            .split(":")
+        return gameNum.toInt() to cubes.parseSubsets()
+    }
+
+    private fun String.parseSubsets(): List<List<Pair<Int, String>>> {
+        return this
+            .trim()
+            .split(";")
+            .map { it.parseSubset() }
+    }
+
+    private fun String.parseSubset(): List<Pair<Int, String>> {
+        return trim()
+            .split(",")
+            .map { cubeString ->
+                cubeString.parseCube()
+            }
+    }
+
+    private fun String.parseCube(): Pair<Int, String> {
+        val (countStr, cubeStr) = this.trim().split(" ")
+        return countStr.toInt() to cubeStr
+    }
+
+    val availableColours = mapOf(
+        "red" to 12,
+        "green" to 13,
+        "blue" to 14,
+    )
 
     private fun compute2(input: List<String>): Int {
         return input.size
