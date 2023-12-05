ff18d46c4e446133ff4f3fd0328b52efd0e438fc
Ranil Wijeyratne <me@ranil.ch>
2023-12-05 20:32:38 +0100

    day05.1

--- src/test/kotlin/ch/ranil/aoc/aoc2023/Day05.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2023/Day05.kt
@@ -8,8 +8,8 @@ class Day05 : AbstractDay() {
 
     @Test
     fun part1() {
-        assertEquals(0, compute1(testInput))
-        assertEquals(0, compute1(puzzleInput))
+        assertEquals(35, compute1(testInput))
+        assertEquals(261668924, compute1(puzzleInput))
     }
 
     @Test
@@ -18,11 +18,90 @@ class Day05 : AbstractDay() {
         assertEquals(0, compute2(puzzleInput))
     }
 
-    private fun compute1(input: List<String>): Int {
-        return input.size
+    private fun compute1(input: List<String>): Long {
+        val (seeds, maps) = parseInput(input)
+        val seedValues = seeds.toMutableList()
+
+        seeds.forEachIndexed { index, element ->
+            var seed = element
+            while (seed.type != "location") {
+                val map = maps.getValue(seed.type)
+                var mapped: Element? = null
+                map.forEach { (range, mapping) ->
+                    val (delta, dstType) = mapping
+                    if (range.contains(seed.value)) {
+                        mapped = Element(dstType, seed.value + delta)
+                        return@forEach
+                    }
+                }
+                seedValues[index] = mapped ?: seed.shift()
+                seed = seedValues[index]
+            }
+        }
+        return seedValues.minOf { it.value }
     }
 
-    private fun compute2(input: List<String>): Int {
-        return input.size
+    private fun compute2(input: List<String>): Long {
+        return input.size.toLong()
     }
+
+    private fun parseInput(input: List<String>): Pair<List<Element>, Map<String, List<Pair<LongRange, Pair<Long, String>>>>> {
+        val seeds = input.first()
+            .split(" ")
+            .drop(1)
+            .map { Element("seed", it.toLong()) }
+
+        mutableMapOf<Pair<String, String>, List<LongRange>>()
+
+        val map = parseMaps(input.drop(2))
+
+        return seeds to map
+    }
+
+    private fun parseMaps(input: List<String>): Map<String, List<Pair<LongRange, Pair<Long, String>>>> {
+        if (input.size <= 1) return emptyMap()
+        val mapInput = input
+            .takeWhile { it.isNotBlank() }
+
+        val restInput = input.drop(mapInput.size + 1)
+        val parsedMap = parseMap(mapInput)
+        return parsedMap + parseMaps(restInput)
+    }
+
+    private fun parseMap(input: List<String>): Map<String, List<Pair<LongRange, Pair<Long, String>>>> {
+        val (srcType, _, dstType) = input.first().split(" ").first().split("-")
+        val mappings = input
+            .drop(1)
+            .takeWhile { it.isNotBlank() }
+            .map { line ->
+                val (dstStart, srcStart, range) = line.split(" ").map { it.toLong() }
+                val delta = dstStart - srcStart
+                srcStart..(srcStart + range) to (delta to dstType)
+            }
+
+        return mapOf(srcType to mappings)
+    }
+
+    data class Element(
+        val type: String,
+        val value: Long,
+    ) {
+        fun shift(): Element {
+            return Element(typeMap.getValue(type), value)
+        }
+
+        companion object {
+            private val typeMap = mapOf(
+                "seed" to "soil",
+                "soil" to "fertilizer",
+                "fertilizer" to "water",
+                "water" to "light",
+                "light" to "temperature",
+                "temperature" to "humidity",
+                "humidity" to "location",
+            )
+        }
+    }
+
+
 }
