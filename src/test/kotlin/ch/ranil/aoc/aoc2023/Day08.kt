7bc39559bbfaf9e08f05dcf619e0cfb8d52d41fc
Ranil Wijeyratne <me@ranil.ch>
2023-12-08 08:22:24 +0100

    day08.1

--- src/test/kotlin/ch/ranil/aoc/aoc2023/Day08.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2023/Day08.kt
@@ -1,6 +1,7 @@
 package ch.ranil.aoc.aoc2023
 
 import ch.ranil.aoc.AbstractDay
+import ch.ranil.aoc.aoc2023.Day08.Direction.*
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -8,12 +9,12 @@ class Day08 : AbstractDay() {
 
     @Test
     fun part1Test() {
-        assertEquals(0, compute1(testInput))
+        assertEquals(2, compute1(testInput))
     }
 
     @Test
     fun part1Puzzle() {
-        assertEquals(0, compute1(puzzleInput))
+        assertEquals(21883, compute1(puzzleInput))
     }
 
     @Test
@@ -27,10 +28,66 @@ class Day08 : AbstractDay() {
     }
 
     private fun compute1(input: List<String>): Int {
-        return input.size
+        val (sequence, map) = parse(input)
+
+        var hops = 0
+        var pos = "AAA"
+        while (pos != "ZZZ") {
+            pos = when (sequence.next()) {
+                LEFT -> map.getValue(pos).first
+                RIGHT -> map.getValue(pos).second
+            }
+            hops++
+        }
+        return hops
     }
 
     private fun compute2(input: List<String>): Int {
         return input.size
+    }
+
+    enum class Direction {
+        LEFT, RIGHT;
+
+        companion object {
+            fun of(c: Char): Direction = when (c) {
+                'R' -> RIGHT
+                'L' -> LEFT
+                else -> throw IllegalArgumentException()
+            }
+        }
+    }
+
+    fun parse(input: List<String>): Pair<Sequence, Map<String, Pair<String, String>>> {
+        val s = Sequence(input.first())
+
+        val m = input
+            .drop(2)
+            .map { it.split(" = ") }
+            .associateBy(
+                keySelector = { (k, _) -> k },
+                valueTransform = { (_, v) ->
+                    val vals = Regex("\\(([A-Z]{3}), ([A-Z]{3})\\)").find(v)!!.groupValues
+                    vals[1] to vals[2]
+                }
+            )
+        return s to m
+    }
+
+    class Sequence(private val s: String) {
+        private var i = 0
+
+        fun next(): Direction {
+            val d = Direction.of(s[i++ % s.length])
+            return d
+        }
+    }
+
+    @Test
+    fun sequenceTest() {
+        val s = Sequence("LR")
+        assertEquals(LEFT, s.next())
+        assertEquals(RIGHT, s.next())
+        assertEquals(LEFT, s.next())
     }
 }
