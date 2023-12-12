5ccdb1fbe7f8b140dda6482b077d9f6c55763184
Ranil Wijeyratne <me@ranil.ch>
2023-12-12 08:45:52 +0100

    day12.1 outline

---
+++ src/test/kotlin/ch/ranil/aoc/aoc2023/Day12.kt
@@ -0,0 +1,62 @@
+package ch.ranil.aoc.aoc2023
+
+import ch.ranil.aoc.AbstractDay
+import org.junit.jupiter.api.Test
+import kotlin.math.abs
+import kotlin.test.assertEquals
+
+class Day12 : AbstractDay() {
+
+    @Test
+    fun part1Test() {
+        assertEquals(1, countArrangements("???.### 1,1,3"))
+        assertEquals(4, countArrangements(".??..??...?##. 1,1,3"))
+        assertEquals(1, countArrangements("?#?#?#?#?#?#?#? 1,3,1,6"))
+        assertEquals(1, countArrangements("????.#...#... 4,1,1"))
+        assertEquals(4, countArrangements("????.######..#####. 1,6,5"))
+        assertEquals(10, countArrangements("?###???????? 3,2,1"))
+    }
+
+    @Test
+    fun part1Puzzle() {
+        assertEquals(0, compute1(puzzleInput))
+    }
+
+    @Test
+    fun part2Test() {
+        assertEquals(0, compute1(testInput))
+    }
+
+    @Test
+    fun part2Puzzle() {
+        assertEquals(0, compute2(puzzleInput))
+    }
+
+    private fun compute1(input: List<String>): Long {
+        return 0
+    }
+
+    private fun compute2(input: List<String>): Long {
+        return 0
+    }
+
+    private fun countArrangements(s: String): Int {
+        val (puzzle, groupCounts) = parse(s)
+
+        getGroupCounts(puzzle)
+        return 0
+    }
+
+    private fun getGroupCounts(puzzle: String): List<Int> {
+        val groups = Regex("(#+)").find(puzzle)?.groupValues.orEmpty().drop(1)
+        puzzle
+        return emptyList()
+    }
+
+    private fun parse(s: String): Pair<String, List<Int>> {
+        val (puzzle, groups) = s.split(" ")
+        val groupCounts = groups.split(",").map { it.toInt() }
+
+        return puzzle to groupCounts
+    }
+}
