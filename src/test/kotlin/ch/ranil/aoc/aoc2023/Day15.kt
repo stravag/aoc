a66e44dd9ee090f308800485a32c529ee7f7ef6f
Ranil Wijeyratne <me@ranil.ch>
2023-12-15 08:48:29 +0100

    Day15.2 (buggy)

--- src/test/kotlin/ch/ranil/aoc/aoc2023/Day15.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2023/Day15.kt
@@ -13,17 +13,17 @@ class Day15 : AbstractDay() {
 
     @Test
     fun part1Test() {
-        assertEquals(516804, compute1(testInput))
+        assertEquals(1320, compute1(testInput))
     }
 
     @Test
     fun part1Puzzle() {
-        assertEquals(110565, compute1(puzzleInput))
+        assertEquals(516804, compute1(puzzleInput))
     }
 
     @Test
     fun part2Test() {
-        assertEquals(0, compute2(testInput))
+        assertEquals(145, compute2(testInput))
     }
 
     @Test
@@ -41,7 +41,31 @@ class Day15 : AbstractDay() {
     }
 
     private fun compute2(input: List<String>): Int {
-        TODO()
+        val boxes = HashMap<Int, LinkedHashMap<String, Int>>()
+
+        input
+            .joinToString("")
+            .split(",")
+            .forEach {
+                val boxNumber = hash(it)
+                if (it.contains("-") && boxes.contains(boxNumber)) {
+                    val label = it.replace("-", "")
+                    boxes.getValue(boxNumber).remove(label)
+                } else if (it.contains("=")){
+                    val (label, focalLengthStr) = it.split("=")
+                    val linkedMap = boxes[boxNumber] ?: LinkedHashMap()
+                    linkedMap[label] = focalLengthStr.toInt()
+                    boxes[boxNumber] = linkedMap
+                }
+            }
+
+        return boxes.entries.sumOf { (boxNumber, data) ->
+            var sumOfBox = 0
+            data.entries.forEachIndexed { index, entry ->
+                sumOfBox += boxNumber * index * entry.value
+            }
+            sumOfBox
+        }
     }
 
     private fun hash(s: String): Int {
