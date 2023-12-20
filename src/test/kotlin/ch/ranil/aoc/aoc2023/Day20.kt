3e813da19d12e67f1ff26130a945fdef6f395ab9
Ranil Wijeyratne <ranil.wijeyratne@ergon.ch>
2023-12-20 08:38:07 +0100

    Day20 wip

--- src/test/kotlin/ch/ranil/aoc/aoc2023/Day20.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2023/Day20.kt
@@ -1,6 +1,8 @@
 package ch.ranil.aoc.aoc2023
 
 import ch.ranil.aoc.AbstractDay
+import ch.ranil.aoc.aoc2023.Day20.Pulse.HIGH
+import ch.ranil.aoc.aoc2023.Day20.Pulse.LOW
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -32,5 +34,62 @@ class Day20 : AbstractDay() {
 
     private fun compute2(input: List<String>): Int {
         TODO()
+    }
+
+    private sealed interface Module {
+        val name: String
+        fun handlePulse(sourceModule: String, pulse: Pulse): Map<String, Pulse>
+    }
+
+    private data class Broadcast(
+        override val name: String,
+        private val targetModules: List<String>,
+    ) : Module {
+        override fun handlePulse(sourceModule: String, pulse: Pulse): Map<String, Pulse> {
+            return targetModules.associateWith { pulse }
+        }
+    }
+
+    private data class FlipFlop(
+        override val name: String,
+        private val targetModule: String,
+        private var on: Boolean = false,
+    ) : Module {
+        override fun handlePulse(sourceModule: String, pulse: Pulse): Map<String, Pulse> {
+            return if (pulse == LOW) {
+                if (on) {
+                    on = false
+                    mapOf(targetModule to LOW)
+                } else {
+                    on = true
+                    mapOf(targetModule to HIGH)
+                }
+            } else {
+                emptyMap()
+            }
+        }
+    }
+
+    private data class Conjunction(
+        override val name: String,
+        private val targetModule: String,
+        private var pulseHistory: MutableMap<String, Pulse> = mutableMapOf(),
+    ) : Module {
+        override fun handlePulse(sourceModule: String, pulse: Pulse): Map<String, Pulse> {
+            pulseHistory[sourceModule] = pulse
+            return if (pulseHistory.values.all { it == HIGH }) {
+                mapOf(targetModule to HIGH)
+            } else {
+                mapOf(targetModule to LOW)
+            }
+        }
+
+        fun trackInput(sourceModule: String) {
+            pulseHistory[sourceModule] = LOW
+        }
+    }
+
+    private enum class Pulse(val bool: Boolean) {
+        LOW(false), HIGH(true)
     }
 }
