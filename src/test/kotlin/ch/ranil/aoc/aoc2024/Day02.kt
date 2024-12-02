44d088e6ab0dae0f0058d15b752eb6e4bcb8dbba
Ranil Wijeyratne <me@ranil.ch>
2024-12-02 07:06:44 +0100

    Day 2024.01

--- src/test/kotlin/ch/ranil/aoc/aoc2024/Day02.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2024/Day02.kt
@@ -8,29 +8,77 @@ class Day02 : AbstractDay() {
 
     @Test
     fun part1Test() {
-        assertEquals(0, compute1(testInput))
+        assertEquals(2, compute1(testInput))
     }
 
     @Test
     fun part1Puzzle() {
-        assertEquals(0, compute1(puzzleInput))
+        assertEquals(224, compute1(puzzleInput))
     }
 
     @Test
     fun part2Test() {
-        assertEquals(0, compute2(testInput))
+        assertEquals(4, compute2(testInput))
     }
 
     @Test
     fun part2Puzzle() {
-        assertEquals(0, compute2(puzzleInput))
+        assertEquals(293, compute2(puzzleInput))
     }
 
     private fun compute1(input: List<String>): Int {
-        TODO()
+        val result = input
+            .map { row -> row.split(" ").map { it.toInt() } }
+            .map { row -> checkSafety(row) }
+
+        return result.count { it }
     }
 
     private fun compute2(input: List<String>): Int {
-        TODO()
+        val result = input
+            .map { row -> row.split(" ").map { it.toInt() } }
+            .map { row -> checkSafetyWithTolerance(row) }
+
+        return result.count { it }
     }
+
+    private fun checkSafety(row: List<Int>): Boolean {
+        val (increasing, decreasing) = areIncreasingDecreasing(row)
+
+        return increasing.all { it } || decreasing.all { it }
+    }
+
+    private fun checkSafetyWithTolerance(row: List<Int>): Boolean {
+        val (increasing, decreasing) = areIncreasingDecreasing(row)
+
+        if (increasing.all { it } || decreasing.all { it }) return true
+
+        if (increasing.count { !it } <= 2) {
+            for (indexToRemove in row.indices) {
+                val rowCandidate = row.filterIndexed { index, _ -> indexToRemove != index }
+                val safe = checkSafety(rowCandidate)
+                if (safe) return true
+            }
+        }
+
+        if (decreasing.count { !it } <= 2) {
+            for (indexToRemove in row.indices) {
+                val rowCandidate = row.filterIndexed { index, _ -> indexToRemove != index }
+                val safe = checkSafety(rowCandidate)
+                if (safe) return true
+            }
+        }
+
+        return false
+    }
+
+    private fun areIncreasingDecreasing(row: List<Int>): Pair<List<Boolean>, List<Boolean>> {
+        val increasing = row.zipWithNext { l, r -> isIncreasing(l, r) }
+        val decreasing = row.zipWithNext { l, r -> isDecreasing(l, r) }
+        return increasing to decreasing
+    }
+
+    private fun isIncreasing(l: Int, r: Int) = (r - l) < 4 && (r - l) > 0
+
+    private fun isDecreasing(l: Int, r: Int) = (l - r) < 4 && (l - r) > 0
 }
