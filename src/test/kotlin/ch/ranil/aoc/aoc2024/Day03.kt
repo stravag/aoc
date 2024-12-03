ecec70a45a718d00453f2584a169bced886f3f1d
Ranil Wijeyratne <me@ranil.ch>
2024-12-03 10:34:20 +0100

    Day 2024.03 simplify

--- src/test/kotlin/ch/ranil/aoc/aoc2024/Day03.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2024/Day03.kt
@@ -28,53 +28,42 @@ class Day03 : AbstractDay() {
         assertEquals(82868252, compute2(puzzleInput))
     }
 
-    private fun compute1(input: List<String>): Int {
-        return input
-            .sumOf { calculate1(it) }
-    }
-
-    private val regex1 = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)".toRegex()
-    private fun calculate1(input: String): Int {
-        val matches = regex1.findAll(input).toList()
-        return matches.sumOf { matchResult ->
-            val (_, i1, i2) = matchResult.groupValues
-            i1.toInt() * i2.toInt()
-        }
+    private fun compute1(input: List<String>): Long {
+        val memory = input.joinToString(separator = "")
+        val regex = "mul\\([0-9]{1,3},[0-9]{1,3}\\)".toRegex()
+        val matches = regex.findAll(memory).map { it.value }
+        return matches.sumOf { calculate(it) }
     }
 
     private fun compute2(input: List<String>): Long {
         val memory = input.joinToString(separator = "")
-        return calculate2(memory)
-    }
-
-    private val regex2 = "(mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|(do\\(\\))|(don't\\(\\)))".toRegex()
-    private fun calculate2(input: String): Long {
-        val matches = regex2.findAll(input).toList()
+        val regex = "(mul\\([0-9]{1,3},[0-9]{1,3}\\)|(do\\(\\))|(don't\\(\\)))".toRegex()
+        val matches = regex.findAll(memory).toList()
         var enabled = true
         var sum = 0L
         for (match in matches) {
             val op = match.groupValues.first()
 
             if (enabled) {
-                printColor(PrintColor.GREEN,"$op ")
+                printColor(PrintColor.GREEN, "$op ")
             } else {
-                printColor(PrintColor.RED,"$op ")
+                printColor(PrintColor.RED, "$op ")
             }
 
             if (op.startsWith("mul") && enabled) {
-                sum += calculate(match)
+                sum += calculate(op)
             } else if (op == "do()") {
                 enabled = true
             } else if (op == "don't()") {
                 enabled = false
             }
         }
-
         return sum
     }
 
-    private fun calculate(matchResult: MatchResult): Long {
-        val (_, _, i1, i2) = matchResult.groupValues
+    private fun calculate(mul: String): Long {
+        val mulRegex = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)".toRegex()
+        val (_, i1, i2) = mulRegex.find(mul)!!.groupValues
         return i1.toLong() * i2.toLong()
     }
 }
