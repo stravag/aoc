437f8420a734409f21d2f3bc966f0af9fbcfa220
Ranil Wijeyratne <me@ranil.ch>
2024-12-03 07:48:26 +0100

    Day 2024.03.2 WIP

--- src/test/kotlin/ch/ranil/aoc/aoc2024/Day03.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2024/Day03.kt
@@ -1,6 +1,8 @@
 package ch.ranil.aoc.aoc2024
 
 import ch.ranil.aoc.AbstractDay
+import ch.ranil.aoc.PrintColor
+import ch.ranil.aoc.printColor
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -13,12 +15,12 @@ class Day03 : AbstractDay() {
 
     @Test
     fun part1Puzzle() {
-        assertEquals(0, compute1(puzzleInput))
+        assertEquals(170778545, compute1(puzzleInput))
     }
 
     @Test
     fun part2Test() {
-        assertEquals(0, compute2(testInput))
+        assertEquals(48, compute2(test2Input))
     }
 
     @Test
@@ -29,20 +31,52 @@ class Day03 : AbstractDay() {
 
     private fun compute1(input: List<String>): Int {
         return input
-            .sumOf { it.parse() }
+            .sumOf { calculate1(it) }
     }
 
-    private val regex = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)".toRegex()
-    private fun String.parse(): Int {
-        return regex
-            .findAll(this)
-            .sumOf { matchResult ->
-                val (_, i1, i2) = matchResult.groupValues
-                i1.toInt() * i2.toInt()
-            }
+    private val regex1 = "mul\\(([0-9]{1,3}),([0-9]{1,3})\\)".toRegex()
+    private fun calculate1(input: String): Int {
+        val matches = regex1.findAll(input).toList()
+        return matches.sumOf { matchResult ->
+            val (_, i1, i2) = matchResult.groupValues
+            i1.toInt() * i2.toInt()
+        }
     }
 
     private fun compute2(input: List<String>): Int {
-        TODO()
+        return input
+            .sumOf { calculate2(it) }
+    }
+
+    private val regex2 = "(mul\\(([0-9]{1,3}),([0-9]{1,3})\\)|(do\\(\\))|(don't\\(\\)))".toRegex()
+    private fun calculate2(input: String): Int {
+        println("Calculating row...")
+        val matches = regex2.findAll(input).toList()
+        var enabled = true
+        var sum = 0
+        for (match in matches) {
+            val op = match.groupValues.first()
+
+            if (enabled) {
+                printColor(PrintColor.GREEN,"$op ")
+            } else {
+                printColor(PrintColor.RED,"$op ")
+            }
+
+            if (op.startsWith("mul") && enabled) {
+                sum += calculate(match)
+            } else if (op.startsWith("do()")) {
+                enabled = true
+            } else if (op.startsWith("don't()")) {
+                enabled = false
+            }
+        }
+
+        return sum
+    }
+
+    private fun calculate(matchResult: MatchResult): Int {
+        val (_, _, i1, i2) = matchResult.groupValues
+        return i1.toInt() * i2.toInt()
     }
 }
