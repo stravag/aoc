d932606d9834e0548096ef4388ce1cff1d55c8d4
Ranil Wijeyratne <me@ranil.ch>
2024-12-08 08:55:12 +0100

    Day 2024.08.2

--- src/test/kotlin/ch/ranil/aoc/aoc2024/Day08.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2024/Day08.kt
@@ -16,17 +16,34 @@ class Day08 : AbstractDay() {
 
     @Test
     fun part1Puzzle() {
-        assertEquals(0, compute1(puzzleInput))
+        assertEquals(398, compute1(puzzleInput))
     }
 
     @Test
     fun part2Test() {
-        assertEquals(0, compute2(testInput))
+        assertEquals(34, compute2(testInput))
+    }
+
+    @Test
+    fun part2Test2() {
+        val dummyInput = """
+            T.........
+            ...T......
+            .T........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+            ..........
+        """.trimIndent().lines()
+        assertEquals(9, compute2(dummyInput))
     }
 
     @Test
     fun part2Puzzle() {
-        assertEquals(0, compute2(puzzleInput))
+        assertEquals(1333, compute2(puzzleInput))
     }
 
     private fun compute1(input: List<String>): Long {
@@ -55,20 +72,70 @@ class Day08 : AbstractDay() {
         return uniqueAntiNodes.size.toLong()
     }
 
-    private fun getAntiNodes(p1: Point, p2: Point): List<Point> {
+    private fun compute2(input: List<String>): Long {
+        val knownAntennas = mutableMapOf<Char, List<Point>>()
+        val uniqueAntiNodes = mutableSetOf<Point>()
+        input.forEachPointWithChar { antenna, frequency ->
+            if (frequency != '.') {
+                val matchingAntennas = knownAntennas[frequency].orEmpty()
+                matchingAntennas.forEach { matchingAntenna ->
+                    val antiNodes = getAntiNodes2(antenna, matchingAntenna, input)
+                    uniqueAntiNodes.addAll(antiNodes)
+                }
+                knownAntennas.compute(frequency) { _, acc -> acc.orEmpty() + antenna }
+            }
+        }
+
+        input.print { point, c ->
+            when {
+                uniqueAntiNodes.contains(point) -> printColor(RED, '#')
+                c == '.' -> print(c)
+                else -> printColor(GREEN, c)
+            }
+        }
+
+        return uniqueAntiNodes.size.toLong()
+    }
+
+    private fun getAntiNodes(p1: Point, p2: Point): Set<Point> {
         val dX = p1.x - p2.x
         val dY = p1.y - p2.y
-        val antiNodes = listOf(
+        val antiNodes = setOf(
             Point(p1.x + dX, p1.y + dY),
             Point(p1.x - dX, p1.y - dY),
             Point(p2.x + dX, p2.y + dY),
             Point(p2.x - dX, p2.y - dY),
         )
-        val antiNodesMinusAntennas = antiNodes.filter { it != p1 && it != p2 }
+        val antiNodesMinusAntennas = antiNodes.minus(p1).minus(p2)
         return antiNodesMinusAntennas
     }
 
-    private fun compute2(input: List<String>): Long {
-        TODO()
+    private fun getAntiNodes2(p1: Point, p2: Point, input: List<String>): Set<Point> {
+        val dX = p1.x - p2.x
+        val dY = p1.y - p2.y
+
+        val antiNodes1 = walkInLine(
+            point = p1,
+            step = { Point(it.x + dX, it.y + dY) },
+            input = input
+        )
+
+        val antiNodes2 = walkInLine(
+            point = p1,
+            step = { Point(it.x - dX, it.y - dY) },
+            input = input
+        )
+
+        return antiNodes1 + antiNodes2 + p1
+    }
+
+    private fun walkInLine(point: Point, step: (Point) -> Point, input: List<String>): Set<Point> {
+        var p = step(point)
+        val pointsInLine = mutableSetOf<Point>()
+        while (input.containsPoint(p)) {
+            pointsInLine.add(p)
+            p = step(p)
+        }
+        return pointsInLine
     }
 }
