708006806b15139256e52eaa901e45611b6324f7
Ranil Wijeyratne <me@ranil.ch>
2024-12-09 07:23:53 +0100

    Day 2024.09.1

--- src/test/kotlin/ch/ranil/aoc/aoc2024/Day09.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2024/Day09.kt
@@ -1,7 +1,11 @@
 package ch.ranil.aoc.aoc2024
 
 import ch.ranil.aoc.common.AbstractDay
+import ch.ranil.aoc.common.PrintColor.GREEN
+import ch.ranil.aoc.common.isEven
+import ch.ranil.aoc.common.printColor
 import org.junit.jupiter.api.Test
+import java.util.LinkedList
 import kotlin.test.assertEquals
 
 class Day09 : AbstractDay() {
@@ -13,7 +17,7 @@ class Day09 : AbstractDay() {
 
     @Test
     fun part1Puzzle() {
-        assertEquals(0, compute1(puzzleInput))
+        assertEquals(6432869891895, compute1(puzzleInput))
     }
 
     @Test
@@ -27,10 +31,57 @@ class Day09 : AbstractDay() {
     }
 
     private fun compute1(input: List<String>): Long {
-        TODO()
+        val blocks = input.single().flatMapIndexed { index, c ->
+            val size = c.digitToInt()
+            if (index.isEven()) {
+                List(size) { Block.File(id = index / 2) }
+            } else {
+                List(size) { Block.Free }
+            }
+        }
+
+        blocks.printBlocks()
+        println()
+
+        var checksum = 0L
+        var processedFileBlocks = 0
+        val fileBlocks = LinkedList(blocks.filterIsInstance<Block.File>())
+
+        for ((index, block) in blocks.withIndex()) {
+            if (processedFileBlocks >= fileBlocks.size) break
+            when (block) {
+                is Block.File -> {
+                    checksum += index * block.id
+                    processedFileBlocks++
+                    print(block.id)
+                }
+
+                is Block.Free -> {
+                    val blockToInsert = fileBlocks.removeLast()
+                    checksum += index * blockToInsert.id
+                    printColor(blockToInsert.id, GREEN)
+                }
+            }
+        }
+
+        return checksum
     }
 
     private fun compute2(input: List<String>): Long {
         TODO()
+    }
+
+    private fun List<Block>.printBlocks() {
+        forEach { block ->
+            when (block) {
+                is Block.File -> print(block.id)
+                is Block.Free -> print(".")
+            }
+        }
+    }
+
+    sealed interface Block {
+        data object Free : Block
+        data class File(val id: Int) : Block
     }
 }
