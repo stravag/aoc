6b1817330dfafa0c62441fece30971451085faa6
Ranil Wijeyratne <me@ranil.ch>
2024-12-19 15:37:11 +0100

    Day 2024.19.2

--- src/test/kotlin/ch/ranil/aoc/aoc2024/Day19.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2024/Day19.kt
@@ -2,6 +2,8 @@ package ch.ranil.aoc.aoc2024
 
 import ch.ranil.aoc.common.AbstractDay
 import ch.ranil.aoc.common.Debug
+import ch.ranil.aoc.common.PrintColor
+import ch.ranil.aoc.common.printlnColor
 import org.junit.jupiter.api.Test
 import java.util.*
 import kotlin.test.assertEquals
@@ -20,14 +22,23 @@ class Day19 : AbstractDay() {
     }
 
     @Test
+    fun part2Dummy() {
+        Debug.enable()
+
+        val desiredDesign = "abcdabcd"
+        val availableTowels = setOf("a", "b", "c", "d", "ab", "bc", "cd", "abc")
+        assertEquals(36, variationsForDesign(desiredDesign, availableTowels))
+    }
+
+    @Test
     fun part2Test() {
         Debug.enable()
-        assertEquals(0, compute2(testInput))
+        assertEquals(16, compute2(testInput))
     }
 
     @Test
     fun part2Puzzle() {
-        assertEquals(0, compute2(puzzleInput))
+        assertEquals(622121814629343, compute2(puzzleInput))
     }
 
     private fun compute1(input: List<String>): Int {
@@ -35,6 +46,14 @@ class Day19 : AbstractDay() {
         return input.drop(2)
             .count { desiredDesign ->
                 designPossible(desiredDesign, towels)
+            }
+    }
+
+    private fun compute2(input: List<String>): Long {
+        val towels = input.first().split(", ").toSet()
+        return input.drop(2)
+            .sumOf { desiredDesign ->
+                variationsForDesign(desiredDesign, towels)
             }
     }
 
@@ -67,7 +86,31 @@ class Day19 : AbstractDay() {
         return false
     }
 
-    private fun compute2(input: List<String>): Long {
-        return input.size.toLong()
+    private fun variationsForDesign(
+        desiredDesign: String,
+        availableTowels: Set<String>,
+        cache: MutableMap<String, Long> = mutableMapOf()
+    ): Long {
+        Debug.debug { println("Processing solution: $desiredDesign") }
+        if (desiredDesign in cache) {
+            Debug.debug { printlnColor("Known solution found", PrintColor.YELLOW) }
+            return cache.getValue(desiredDesign)
+        }
+        if (desiredDesign.isEmpty()) {
+            Debug.debug { printlnColor("Found solution", PrintColor.GREEN) }
+            return 1
+        }
+        val variations = availableTowels
+            .filter { desiredDesign.startsWith(it) }
+            .sumOf { towel ->
+                val remainingDesign = desiredDesign.substringAfter(towel)
+                variationsForDesign(
+                    desiredDesign = remainingDesign,
+                    availableTowels = availableTowels,
+                    cache = cache
+                )
+            }
+        cache[desiredDesign] = variations
+        return variations
     }
 }
