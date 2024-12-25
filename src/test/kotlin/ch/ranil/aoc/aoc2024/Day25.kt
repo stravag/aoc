46d5a2448708ebce0e11b9c8a8f23e9cb5c33443
Ranil Wijeyratne <me@ranil.ch>
2024-12-25 16:23:23 +0100

    Day 2024.25.1

--- src/test/kotlin/ch/ranil/aoc/aoc2024/Day25.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2024/Day25.kt
@@ -2,6 +2,9 @@ package ch.ranil.aoc.aoc2024
 
 import ch.ranil.aoc.common.AbstractDay
 import ch.ranil.aoc.common.Debug
+import ch.ranil.aoc.common.allPoints
+import ch.ranil.aoc.common.charFor
+import ch.ranil.aoc.common.types.Point
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -10,12 +13,12 @@ class Day25 : AbstractDay() {
     @Test
     fun part1Test() {
         Debug.enable()
-        assertEquals(0, compute1(testInput))
+        assertEquals(3, compute1(testInput))
     }
 
     @Test
     fun part1Puzzle() {
-        assertEquals(0, compute1(puzzleInput))
+        assertEquals(3021, compute1(puzzleInput))
     }
 
     @Test
@@ -29,11 +32,52 @@ class Day25 : AbstractDay() {
         assertEquals(0, compute2(puzzleInput))
     }
 
-    private fun compute1(input: List<String>): Long {
-        return input.size.toLong()
+    private fun compute1(input: List<String>): Int {
+        val (locks, keys) = parse(input)
+
+        return locks
+            .flatMap { lock -> keys.map { key -> lock.matches(key) } }
+            .count { it }
     }
 
     private fun compute2(input: List<String>): Long {
         return input.size.toLong()
     }
+
+    private fun parse(input: List<String>): Pair<Set<Lock>, Set<Key>> {
+        fun <T> build(input: List<String>, constructor: (Set<Point>) -> T): T {
+            val data = input
+                .allPoints()
+                .filter { input.charFor(it) == '#' }
+                .toSet()
+            return constructor(data)
+        }
+
+        val locks = mutableSetOf<Lock>()
+        val keys = mutableSetOf<Key>()
+
+        var offset = 0
+        while (offset < input.size) {
+            val next = input
+                .drop(offset)
+                .takeWhile { it.isNotBlank() }
+
+            when (next.first()) {
+                "#####" -> build(next, ::Lock).also { locks.add(it) }
+                "....." -> build(next, ::Key).also { keys.add(it) }
+            }
+
+            offset += next.size + 1
+        }
+
+        return locks to keys
+    }
+
+    private data class Lock(val data: Set<Point>) {
+        fun matches(key: Key): Boolean {
+            val hasOverlaps = this.data.intersect(key.data).isNotEmpty()
+            return !hasOverlaps
+        }
+    }
+    private data class Key(val data: Set<Point>)
 }
