28546e55506bd1acecf38ab991ff7b14f1f3699a
Ranil Wijeyratne <me@ranil.ch>
2025-12-01 22:02:24 +0100

    Day 2025.01.2

--- src/test/kotlin/ch/ranil/aoc/aoc2025/Day01.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2025/Day01.kt
@@ -8,48 +8,51 @@ class Day01 : AbstractDay() {
 
     @Test
     fun part1() {
-        assertEquals(
-            3, compute1(
-                """
-                L68
-                L30
-                R48
-                L5
-                R60
-                L55
-                L1
-                L99
-                R14
-                L82
-            """.trimIndent().lines()
-            )
-        )
+        assertEquals(3, compute1(testInput))
         assertEquals(1135, compute1(puzzleInput))
     }
 
     @Test
     fun part2() {
+        assertEquals(6, compute2(testInput))
+        assertEquals(6558, compute2(puzzleInput))
     }
 
     private fun compute1(input: List<String>): Int {
         var count = 0
         var position = 50
-        input.map {
-            val operand = it.operand()
+        input.forEach {
             val steps = it.drop(1).toInt()
+            position = if (it.first() == 'L') {
+                position - steps
+            } else {
+                position + steps
+            } % 100
 
-            position = operand(position, steps) % 100
             if (position == 0) count++
         }
         return count
     }
 
-    private fun String.operand(): (Int, Int) -> Int {
-        val operand: (Int, Int) -> Int = when (this.first()) {
-            'L' -> Int::minus
-            'R' -> Int::plus
-            else -> throw IllegalArgumentException(this)
+    private fun compute2(input: List<String>): Int {
+        var count = 0
+        var position = 50
+
+        input.forEach { line ->
+            val n = line.drop(1).toInt()
+            val turns = n % 100
+            val newPosition: Int
+            count += n / 100
+            if (line.first() == 'L') {
+                newPosition = (position - turns + 100) % 100
+                if ((newPosition > position || newPosition == 0) && position != 0) count++
+            } else {
+                newPosition = (position + turns) % 100
+                if (newPosition < position) count++
+            }
+            position = newPosition
         }
-        return operand
+        return count
     }
+
 }
