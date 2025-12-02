f5f8560a2c1b6c88f1096d75e01826b7477c631e
Ranil Wijeyratne <me@ranil.ch>
2025-12-02 07:28:24 +0100

    Day 2025.02.1

--- src/test/kotlin/ch/ranil/aoc/aoc2025/Day02.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2025/Day02.kt
@@ -9,7 +9,11 @@ class Day02 : AbstractDay() {
     @Test
     fun part1() {
         assertEquals(1227775554, compute1(testInput))
-        assertEquals(-1, compute1(puzzleInput))
+    }
+
+    @Test
+    fun part1Puzzle() {
+        assertEquals(18952700150, compute1(puzzleInput))
     }
 
     @Test
@@ -19,11 +23,30 @@ class Day02 : AbstractDay() {
     }
 
     private fun compute1(input: List<String>): Long {
-        return 0
+        return input
+            .parsePairs()
+            .flatMap { (l, r) -> (l..r).filter { it.isInvalid() } }
+            .sum()
     }
 
     private fun compute2(input: List<String>): Long {
         return 0
     }
 
+    private fun List<String>.parsePairs(): List<Pair<Long, Long>> {
+        return this
+            .joinToString(separator = "")
+            .let { line -> line.split(",").map { pairString -> pairString.split("-") } }
+            .map { (a, b) -> a.toLong() to b.toLong() }
+    }
+
+    private fun Long.isInvalid(): Boolean {
+        val s = this.toString()
+        val len = s.length
+        if (len % 2 != 0) return false
+
+        val l = s.substring(0..<(len / 2))
+        val r = s.substring(len / 2)
+        return l == r
+    }
 }
