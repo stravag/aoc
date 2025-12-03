7cbb6671c4233988761d9b0b32eadb5c88c8db76
Ranil Wijeyratne <me@ranil.ch>
2025-12-03 22:28:21 +0100

    Day 2025.03.2

--- src/test/kotlin/ch/ranil/aoc/aoc2025/Day03.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2025/Day03.kt
@@ -20,19 +20,20 @@ class Day03 : AbstractDay() {
 
     @Test
     fun part2() {
-        assertEquals(-1, compute2(testInput))
+        Debug.enable()
+        assertEquals(3121910778619, compute2(testInput))
     }
 
     @Test
     fun part2Puzzle() {
-        assertEquals(-1, compute2(puzzleInput))
+        assertEquals(171039099596062, compute2(puzzleInput))
     }
 
     private fun compute1(input: List<String>): Long {
-        return input.sumOf { it.maxNum() }
+        return input.sumOf { it.maxNum1() }
     }
 
-    private fun String.maxNum(): Long {
+    private fun String.maxNum1(): Long {
         var maxNum = 0
         for (l in 0 until length - 1) {
             for (r in l + 1 until length) {
@@ -46,6 +47,19 @@ class Day03 : AbstractDay() {
     private fun String.numFor(idxL: Int, idxR: Int) = get(idxL).digitToInt() * 10 + get(idxR).digitToInt()
 
     private fun compute2(input: List<String>): Long {
-        return 0
+        return input.sumOf { it.maxNum2(12) }
+    }
+
+    private fun String.maxNum2(n: Int): Long {
+        var leftPointer = 0
+        var numString = ""
+        (0 until n).forEach { i ->
+            val candidates = substring(leftPointer, length - n + i + 1)
+            val char = candidates.toList().max()
+            numString += char
+            leftPointer = indexOf(char, leftPointer) + 1
+        }
+
+        return numString.toLong()
     }
 }
