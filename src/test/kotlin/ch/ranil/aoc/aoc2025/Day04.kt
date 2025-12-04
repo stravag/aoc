086abf1232e80a4f4b138496d66fd06489d8a374
Ranil Wijeyratne <me@ranil.ch>
2025-12-04 07:36:48 +0100

    Day 2025.04.1

--- src/test/kotlin/ch/ranil/aoc/aoc2025/Day04.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2025/Day04.kt
@@ -2,6 +2,14 @@ package ch.ranil.aoc.aoc2025
 
 import ch.ranil.aoc.common.AbstractDay
 import ch.ranil.aoc.common.Debug
+import ch.ranil.aoc.common.PrintColor
+import ch.ranil.aoc.common.charFor
+import ch.ranil.aoc.common.charForOrNull
+import ch.ranil.aoc.common.containsPoint
+import ch.ranil.aoc.common.forEachPointWithChar
+import ch.ranil.aoc.common.print
+import ch.ranil.aoc.common.printColor
+import ch.ranil.aoc.common.types.Point
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
@@ -15,7 +23,7 @@ class Day04 : AbstractDay() {
 
     @Test
     fun part1Puzzle() {
-        assertEquals(-1, compute1(puzzleInput))
+        assertEquals(1384, compute1(puzzleInput))
     }
 
     @Test
@@ -30,7 +38,27 @@ class Day04 : AbstractDay() {
     }
 
     private fun compute1(input: List<String>): Long {
-        return input.size.toLong()
+        val paperPoints = mutableSetOf<Point>()
+        input.forEachPointWithChar { point, char ->
+            if (char == '@') {
+                val paper = point
+                    .edges()
+                    .filter { e -> input.charForOrNull(e) == '@' }
+                if (paper.size < 4) paperPoints.add(point)
+            }
+        }
+
+        Debug.debug {
+            input.print { p, c ->
+                if (paperPoints.contains(p)) {
+                    printColor("x", PrintColor.YELLOW)
+                } else {
+                    print(c)
+                }
+            }
+        }
+
+        return paperPoints.size.toLong()
     }
 
     private fun compute2(input: List<String>): Long {
