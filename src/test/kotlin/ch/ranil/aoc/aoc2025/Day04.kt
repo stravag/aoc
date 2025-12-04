af2d49cabe9c83184038ebf5c312873b457c5870
Ranil Wijeyratne <me@ranil.ch>
2025-12-04 07:53:43 +0100

    Day 2025.04.2

--- src/test/kotlin/ch/ranil/aoc/aoc2025/Day04.kt
+++ src/test/kotlin/ch/ranil/aoc/aoc2025/Day04.kt
@@ -3,12 +3,11 @@ package ch.ranil.aoc.aoc2025
 import ch.ranil.aoc.common.AbstractDay
 import ch.ranil.aoc.common.Debug
 import ch.ranil.aoc.common.PrintColor
-import ch.ranil.aoc.common.charFor
 import ch.ranil.aoc.common.charForOrNull
-import ch.ranil.aoc.common.containsPoint
 import ch.ranil.aoc.common.forEachPointWithChar
 import ch.ranil.aoc.common.print
 import ch.ranil.aoc.common.printColor
+import ch.ranil.aoc.common.types.AbstractMap
 import ch.ranil.aoc.common.types.Point
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
@@ -29,40 +28,60 @@ class Day04 : AbstractDay() {
     @Test
     fun part2() {
         Debug.enable()
-        assertEquals(-1, compute2(testInput))
+        assertEquals(43, compute2(testInput))
     }
 
     @Test
     fun part2Puzzle() {
-        assertEquals(-1, compute2(puzzleInput))
+        assertEquals(8013, compute2(puzzleInput))
     }
 
     private fun compute1(input: List<String>): Long {
-        val paperPoints = mutableSetOf<Point>()
-        input.forEachPointWithChar { point, char ->
-            if (char == '@') {
-                val paper = point
-                    .edges()
-                    .filter { e -> input.charForOrNull(e) == '@' }
-                if (paper.size < 4) paperPoints.add(point)
-            }
-        }
-
-        Debug.debug {
-            input.print { p, c ->
-                if (paperPoints.contains(p)) {
-                    printColor("x", PrintColor.YELLOW)
-                } else {
-                    print(c)
-                }
-            }
-        }
-
-        return paperPoints.size.toLong()
+        val map = Map(input)
+        return map.removeRolls()
     }
 
     private fun compute2(input: List<String>): Long {
-        return input.size.toLong()
+        var result = 0L
+        val map = Map(input)
+        do {
+            val removed = map.removeRolls()
+            result += removed
+        } while (removed > 0)
+        return result
     }
 
+    private class Map(input: List<String>) : AbstractMap(input) {
+        private val paperRolls = mutableSetOf<Point>()
+
+        init {
+            this.forEach { point, char ->
+                if (char == '@') paperRolls.add(point)
+            }
+        }
+
+        fun removeRolls(): Long {
+            val removableRolls = mutableSetOf<Point>()
+            paperRolls.forEach { point ->
+                val paper = point
+                    .edges()
+                    .filter { e -> paperRolls.contains(e) }
+                if (paper.size < 4) removableRolls.add(point)
+            }
+
+            Debug.debug {
+                printMap { p, c ->
+                    if (removableRolls.contains(p)) {
+                        printColor("x", PrintColor.YELLOW)
+                    } else {
+                        val c = if (paperRolls.contains(p)) '@' else '.'
+                        print(c)
+                    }
+                }
+            }
+
+            paperRolls.removeAll(removableRolls)
+            return removableRolls.size.toLong()
+        }
+    }
 }
